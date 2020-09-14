7bfe17b87a6ff6c7ce541373056b46c57533d1dc
Evan Czyzycki <eczy3826@gmail.com>
2020-09-14 13:29:23 -0700

    added backup and s3 sync

---
+++ backup/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Redmine database
+PGPASSWORD=${REDMINE_PASSWORD} /usr/bin/pg_dump -U ${REDMINE_USERNAME} -h redmine_db -Fc --file=/backup/redmine/redmine.sqlc redmine
+
+# Redmine attachments
+rsync -a /data/redmine/files /backup/redmine
+
+cd /backup
+tar -czvf /backup/archives/redmine_`date +%Y-%m-%d`.tar.gz redmine
---
+++ s3sync/s3sync.sh
@@ -0,0 +1,1 @@
+aws s3 sync /aws s3://nova-backup
