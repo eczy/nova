421321df017b7f91daeb65303631cea4ae507f4a
Evan Czyzycki <eczy3826@gmail.com>
2020-09-21 10:40:53 -0700

    fix jenkins backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,6 +7,8 @@ PGPASSWORD=${REDMINE_PASSWORD} /usr/bin/pg_dump -U ${REDMINE_USERNAME} -h redmin
 # Redmine attachments
 rsync -a /data/redmine/files /backup/redmine
 
+rsync -a /data/jenkins /backup/jenkins
+
 cd /backup
 tar -czvf /backup/archives/redmine_`date +%Y-%m-%d`.tar.gz redmine
 tar -czvf /backup/archives/jenkins_`date +%Y-%m-%d`.tar.gz jenkins
